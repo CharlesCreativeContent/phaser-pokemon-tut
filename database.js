a7dcd03e3907daebc0e3974a709da9cdd75fdf2e
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2023-04-13 13:14:07 -0400

    Update database.js

--- database.js
+++ database.js
@@ -3,6 +3,6 @@ let USERNAME = process.env.DATABASE_USERNAME
 let PASSWORD = process.env.DATABASE_PASSWORD
 module.exports = {
     
-    'url' : `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.vkd0p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.vkd0p.mongodb.net/?retryWrites=true&w=majority`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     'dbName': 'myFirstDatabase'
 };
